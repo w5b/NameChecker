7426b7661fcd6b8b0c36aa6f19bbb369d40ffb3d
w5b <30703153+w5b@users.noreply.github.com>
2022-08-01 07:04:08 +0300

    Update namechecker.py

--- namechecker.py
+++ namechecker.py
@@ -30,7 +30,6 @@ def input_q():
 
 
 def get_random_string(length):
-    # With combination of lower and upper case
     result_str = ''.join(random.choice(string.ascii_letters)
                          for i in range(length))
     return result_str
